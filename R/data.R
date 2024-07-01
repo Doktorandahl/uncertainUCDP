8a89aae271dfde2630c04800e611d737ff174a50
doktorandahl <david.randahl@pcr.uu.se>
2024-07-01 10:10:53 +0200

    final bugfixes

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' UCDP Georeferenced Event Dataset (GED) sample
 #'
-#' This dataset contains a sample of the UCDP Georeferenced Event Dataset (GED) from the 2023 data release. The UCDP GED contains information about conflict events in the world. The dataset contains information about the date, location, and type of conflict events. The dataset is a sample of the full dataset, which can be downloaded from the UCDP website https://ucdp.uu.se/downloads/.
+#' A sample of the UCDP Georeferenced Event Dataset (GED) from the 2023 data release. The data contains information about the date, location, and type of conflict events. The data is a sample of the full dataset, which can be downloaded from the UCDP website <https://ucdp.uu.se/downloads/>.
 #'
 #' @format a tibble with 1000 rows and 49 columns
 #'
--- R/uncertainUCDP.R
+++ R/uncertainUCDP.R
@@ -3,14 +3,15 @@
 #' Parameter extraction for uncertainUCDP-functions
 #'
 #' @description
-#' Function for extracting parameters for the reported-value inflated Gumbel mixture distribution for UCDP events. Primarily intended for internal use by the uncertainUCDP-functions, but can be used to extract parameters for the distribution manually.
+#' Extracting parameters for the reported-value inflated Gumbel mixture distribution for UCDP events. Primarily intended for internal use by the uncertainUCDP-functions, but can be used to extract parameters for the distribution manually.
 #'
 #' @param fatalities A vector of non-negative integers representing the number of fatalities of the UCDP event. Non-integer values are allowed but should be considered experimental
 #' @param tov A character string or integer value representing the type of violence of the UCDP. Must be one of "sb", "ns", "os", or "any" or their numeric equivalent The options are:
-#' - "sb" or 1 for state-based violence
-#' - "ns" or 2 for non-state violence
-#' - "os" or 3 for one-sided violence
-#' - "any" or 4 for parameters estimated across all type of violence. This is somewhat experimental and should be used with caution. This is possibly useful when the type of violence is unknown or when the user wants to combine all types of violence into a single category.
+#'
+#' * "sb" or 1 for state-based violence
+#' * "ns" or 2 for non-state violence
+#' * "os" or 3 for one-sided violence
+#' * "any" or 4 for parameters estimated across all type of violence. This is somewhat experimental and should be used with caution. This is possibly useful when the type of violence is unknown or when the user wants to combine all types of violence into a single category.
 #'
 #' @return A list with three elements: loc, scale, and w. loc and scale are the location and scale parameters of the Gumbel distribution, respectively. w is the weight parameter for the reported-value inflation
 #'
@@ -100,7 +101,7 @@ uncertainUCDP_parameters <- function(fatalities, tov){
 #' Parametric uncertainty distributions for UCDP events
 #'
 #' @description
-#' Density, distribution function, quantile function and random generation for the parametric reported-value inflated Gumbel mixture distribution for UCDP events. The functions estimate the parameters of the distribution based on the number of fatalities and the type of violence of the UCDP event.
+#' Density, distribution, quantile and random number generation functions for the parametric reported-value inflated Gumbel mixture distribution for UCDP events. The functions estimate the parameters of the distribution based on the number of fatalities and the type of violence of the UCDP event.
 #'
 #' @details
 #' The reported-value inflated Gumbel mixture distribution is a parametric distribution for modeling the uncertainty in the number of fatalities of UCDP events. The distribution is a mixture of a Gumbel distribution and a point mass at the reported number of fatalities. The distribution is estimated based on the number of fatalities and the type of violence of the UCDP event. The distribution is estimated using a set of regression models that estimate the location, scale, and weight parameters of the distribution based on the number of fatalities and the type of violence of the UCDP event.
@@ -110,16 +111,18 @@ uncertainUCDP_parameters <- function(fatalities, tov){
 #' @param p Vector of probabilities
 #' @param fatalities A vector of non-negative integers representing the number of fatalities of the UCDP events. Non-integer values are allowed but should be considered experimental.
 #' @param tov A character string representing the type of violence of the UCDP. Must be one of "sb", "ns", "os", or "any". The options are:
-#' - "sb" for state-based violence
-#' - "ns" for non-state violence
-#' - "os" for one-sided violence
-#' - "any" for parameters estimated across all type of violence. This is somewhat experimental and should be used with caution. This is possibly useful when the type of violence is unknown or when the user wants to combine all types of violence into a single category.
+#'
+#' * "sb" for state-based violence
+#' * "ns" for non-state violence
+#' * "os" for one-sided violence
+#' * "any" for parameters estimated across all type of violence. This is somewhat experimental and should be used with caution. This is possibly useful when the type of violence is unknown or when the user wants to combine all types of violence into a single category.
 #'
 #' @return
-#' - \code{duncertainUCDP} gives the density function
-#' - \code{puncertainUCDP} gives the distribution function
-#' - \code{quncertainUCDP} gives the quantile function
-#' - \code{runcertainUCDP} generates random values as a vector of length \code{n}
+#'
+#' * \code{duncertainUCDP} gives the density function
+#' * \code{puncertainUCDP} gives the distribution function
+#' * \code{quncertainUCDP} gives the quantile function
+#' * \code{runcertainUCDP} generates random values as a vector of length \code{n}
 #' @export
 #'
 #' @examples
@@ -196,18 +199,19 @@ quncertainUCDP <- function(p, fatalities, tov = c('sb','ns','os','any')){
 }
 
 
-#' Function for calculating the mean, median, and quantiles of the parametric uncertainty distributions for UCDP events
+#' Mean, median, and quantiles of the parametric uncertainty distributions for UCDP events
 #'
 #' @description
-#' These fuctions calculate the mean, median, and quantiles of the parametric uncertainty distributions for UCDP events. The parametric uncertainty distributions are based on the reported-value inflation Gumbel mixture distribution. The \code{median} and \code{quantile} functions are shortcuts for the \code{quncertainUCDP} function.
+#' Mean, median, and quantiles of the parametric uncertainty distributions for UCDP events. The parametric uncertainty distributions are based on the reported-value inflation Gumbel mixture distribution. The \code{median} and \code{quantile} functions are shortcuts for the \code{quncertainUCDP} function.
 #'
 #'
 #' @param fatalities A vector of non-negative integers representing the number of fatalities of the UCDP events. Non-integer values are allowed but should be considered experimental.
 #' @param tov A character string representing the type of violence of the UCDP. Must be one of "sb", "ns", "os", or "any". The options are:
-#' - "sb" for state-based violence
-#' - "ns" for non-state violence
-#' - "os" for one-sided violence
-#' - "any" for parameters estimated across all type of violence. This is somewhat experimental and should be used with caution. This is possibly useful when the type of violence is unknown or when the user wants to combine all types of violence into a single category.
+#'
+#' * "sb" for state-based violence
+#' * "ns" for non-state violence
+#' * "os" for one-sided violence
+#' * "any" for parameters estimated across all type of violence. This is somewhat experimental and should be used with caution. This is possibly useful when the type of violence is unknown or when the user wants to combine all types of violence into a single category.
 #' @param probs A numeric vector of probabilities with values in [0,1]. The quantiles to calculate.
 #'
 #'
